5deb56349146b3a0f999ab8d6cd2c6560f64763d
ebajec <tacoman164@gmail.com>
2025-08-22 15:44:09 -0400

    add implot to config script

--- configure.sh
+++ configure.sh
@@ -1,2 +1,3 @@
 git clone git@github.com:KhronosGroup/SPIRV-Reflect.git ./deps/spirv-reflect/spirv-reflect
 git clone git@github.com:ocornut/imgui.git ./deps/imgui/imgui
+git clone git@github.com:epezent/implot.git ./deps/imgui/implot
