a48c7fa116032aaed77a2cb84d419863afe5f121
ebajec <tacoman164@gmail.com>
2025-09-18 16:46:29 -0400

    Simpler setup

--- configure.sh
+++ configure.sh
@@ -1,3 +1,5 @@
 git clone git@github.com:KhronosGroup/SPIRV-Reflect.git ./deps/spirv-reflect/spirv-reflect
 git clone git@github.com:ocornut/imgui.git ./deps/imgui/imgui
 git clone git@github.com:epezent/implot.git ./deps/imgui/implot
+git clone git@github.com:nothings/stb.git ./deps/stb/stb
+
