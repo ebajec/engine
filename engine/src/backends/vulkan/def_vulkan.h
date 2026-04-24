30aa8d2e3cf70b37e9922b2910b75988f50ef13d
ebajec <tacoman164@gmail.com>
2026-04-24 19:41:22 -0400

    barebones vulkan initialization code - no actual functionality yet

---
+++ engine/src/backends/vulkan/def_vulkan.h
@@ -0,0 +1,10 @@
+#ifndef EV2_DEF_VULKAN_H
+#define EV2_DEF_VULKAN_H
+
+// vulkan
+#include <vulkan/vk_platform.h>
+#include <vulkan/vulkan_core.h>
+
+#define VK_API_VERSION VK_API_VERSION_1_3
+
+#endif
